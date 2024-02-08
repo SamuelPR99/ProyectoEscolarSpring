22307a49cce9f4c2d854c5cf1726e38bfe160813
Paula <145429233+paulaescudero6@users.noreply.github.com>
2024-02-08 09:45:07 +0100

    2

--- src/main/java/com/daw/proyectoescolar/servicios/tareas/Alumno.java
+++ src/main/java/com/daw/proyectoescolar/servicios/tareas/Alumno.java
@@ -3,7 +3,15 @@ package com.daw.proyectoescolar.servicios.tareas;
 public class Alumno {
 
 	public Alumno() {
-		// TODO Auto-generated constructor stub
+		
+		//ATRIBUTOS
+		
+		//CONSTRUCTORES
+		
+		//METODOS
+		
+		
+		
 	}
 
 }
