783aa46fba8b4d0548c25f3bf81bf3aea8cbac46
SamuelPR99 <145430725+SamuelPR99@users.noreply.github.com>
2024-05-10 19:35:32 +0200

    properties

--- src/main/java/com/daw/proyectoescolar/repositorio/Constantes.java
+++ src/main/java/com/daw/proyectoescolar/repositorio/Constantes.java
@@ -1,38 +1,78 @@
 package com.daw.proyectoescolar.repositorio;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Constantes {
-	
+
 	private Constantes() {
 
 	}
-	
+
 	public static final String ALUMNO = "Alumno";
 	public static final String PROFESOR = "Profesor";
 	public static final String ADMINISTRADOR = "Administrador";
-	
+
 	public static final String VALID = "Tipo de usuario no valido.";
 	public static final String NUM_TAREA_VALID = "Numero de tarea no valido.";
 	public static final String NUM_SELEC = " Numero seleccionado: ";
 	public static final String ERROR_ESCRIBIR_ARCHIVO = "Error al escribir en el archivo: ";
 	public static final String ERROR_LEER_ARCHIVO = "Error al leer el archivo: ";
-	
-	public static final String RUTA_USUARIOS = "src/main/java/com/daw/proyectoescolar/repositorio/usuarios.txt";
-	public static final String RUTA_INCIDENCIAS = "src/main/java/com/daw/proyectoescolar/repositorio/incidencias.txt";
-	public static final String RUTA_TAREAS = "src/main/java/com/daw/proyectoescolar/repositorio/tareas.txt";
-	public static final String RUTA_TEMAS = "src/main/java/com/daw/proyectoescolar/repositorio/temas.txt";
-	public static final String CARPETA_LOGS = "logs/";
-	
+
+	public static final String RUTA_RUTAS_PROPERTIES = "rutas.properties";
+	public static final String RUTA_USUARIOS;
+	public static final String RUTA_INCIDENCIAS;
+	public static final String RUTA_TAREAS;
+	public static final String RUTA_TEMAS;
+	public static final String CARPETA_LOGS;
+	public static final String RUTA_BBDD_PROPERTIES;
+
 	public static final String MENU_PRINCIPAL = "Menu Principal";
 	public static final String MENU_ALUMNOS = "Menu Alumnos";
 	public static final String MENU_PROFESORES = "Menu Profesores";
 	public static final String MENU_ADMINISTRADORES = "Menu Administradores";
-	
-	
+
 	public static final String AVANZA = "Avanzada";
 	public static final String BASICA = "Basica";
 	public static final String INTERMEDIA = "Intermedia";
-	
-	public static final String CONEXION_URL = "jdbc:mariadb://localhost:3306/iesmurcia";
-	public static final String USER = "root";
-	public static final String PASSWORD = "root";
+
+	public static final String CONEXION_URL;
+	public static final String USER;
+	public static final String PASSWORD;
+
+	// Sacar rutas de un archivo .properties
+	static {
+		Properties rutas = new Properties();
+		try {
+			rutas.load(new FileInputStream(RUTA_RUTAS_PROPERTIES));
+
+			RUTA_USUARIOS = rutas.getProperty("ruta.usuarios");
+			RUTA_INCIDENCIAS = rutas.getProperty("ruta.incidencias");
+			RUTA_TAREAS = rutas.getProperty("ruta.tareas");
+			RUTA_TEMAS = rutas.getProperty("ruta.temas");
+			CARPETA_LOGS = rutas.getProperty("carpeta.logs");
+			RUTA_BBDD_PROPERTIES = rutas.getProperty("ruta.bbdd");
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
+	// Sacar datos de la base de datos de un archivo .properties
+	static {
+
+		Properties datosBBDD = new Properties();
+
+		try {
+			datosBBDD.load(new FileInputStream(RUTA_BBDD_PROPERTIES));
+
+			CONEXION_URL = datosBBDD.getProperty("database.url");
+			USER = datosBBDD.getProperty("database.username");
+			PASSWORD = datosBBDD.getProperty("database.password");
+
+		} catch (IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+
 }
