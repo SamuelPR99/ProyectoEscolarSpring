d8f35b4a8c2a4f881bcf7d084c5f243fe212d89a
SamuelPR99 <145430725+SamuelPR99@users.noreply.github.com>
2024-05-10 19:02:02 +0200

    Controladores

---
+++ src/main/java/com/daw/proyectoescolar/controladores/ControladorWeb.java
@@ -0,0 +1,10 @@
+package com.daw.proyectoescolar.controladores;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControladorWeb {
+	
+	// Clase para mappear a traves de ModelAndView
+
+}
